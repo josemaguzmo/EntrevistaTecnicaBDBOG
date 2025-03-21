235c76bbcf825c650ea74db14cdc573874d54b42
josemaguzmo <90878044+josemaguzmo@users.noreply.github.com>
2025-03-20 21:01:10 -0500

    Create ConsultaYview.sql

---
+++ ConsultaYview.sql
@@ -0,0 +1,25 @@
+/* consulta para identificar los clientes más relevantes */
+
+SELECT 
+		c.id_cliente,
+		c.nombre,
+		SUM(f.monto) AS total_transacciones,
+		COUNT(f.id_transaccion) AS cantidad_transacciones
+	FROM fact_transacciones f
+	JOIN dim_clientes c ON f.id_cliente = c.id_cliente
+	GROUP BY c.id_cliente, c.nombre
+	ORDER BY total_transacciones DESC
+	LIMIT 10;
+
+
+/* view para visualización y analisis */
+
+	CREATE OR REPLACE VIEW vw_clientes_rentables AS
+		SELECT 
+			c.id_cliente,
+			c.nombre,
+			SUM(f.monto) AS total_transacciones,
+			COUNT(f.id_transaccion) AS cantidad_transacciones
+		FROM fact_transacciones f
+		JOIN dim_clientes c ON f.id_cliente = c.id_cliente
+		GROUP BY c.id_cliente, c.nombre;
